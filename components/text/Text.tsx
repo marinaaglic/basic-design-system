7d3aba68e86b864852ebabaa53553fdbb7975a0a
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-08-14 16:14:45 +0200

    changed prettier config and added text type

--- components/text/Text.tsx
+++ components/text/Text.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function Text() {
   return (
     <div>Text</div>
---
+++ components/text/Text.types.ts
@@ -0,0 +1,13 @@
+export type TextType =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'default-strong'
+  | 'default-regular'
+  | 'medium-strong'
+  | 'small-strong'
+  | 'label-default'
+  | 'label-small'
+  | 'tab-active'
+  | 'tab-inactive'
+  | 'tab';
