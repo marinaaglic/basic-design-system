f98b75ad7c8bfbc9a0b5ee725e3f9f4ae5e4fa91
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-08-14 18:03:37 +0200

    imported google font

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import Text from '@/components/text/Text'
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Home page</h1>
-      <Text type="h1">Hej</Text>
+      <Text type="h1">Home page</Text>
     </div>
   )
 }
