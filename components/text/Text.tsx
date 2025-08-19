0fa148994b914ab5fc6ebc0136a0bef7e5a82a58
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-08-19 17:00:23 +0200

    added sass and mixin file for styling

--- components/text/Text.tsx
+++ components/text/Text.tsx
@@ -1,5 +1,5 @@
 import { ITextProps } from "./Text.types"
-import styles from "./Text.module.css";
+import styles from "./Text.module.scss";
 
 export default function Text({size, children, className, component = "span", ...rest}: ITextProps) {
   const Tag = component;
