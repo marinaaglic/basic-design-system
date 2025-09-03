593e96c9f7a03d389e3b1fab6d2fe7ddba9d3188
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-09-03 16:43:46 +0200

    removed toggle button from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import styles from './page.module.css';
 import Text from '@/components/text/Text';
 import Button from '@/components/button/Button';
-import ToggleButton from '@/components/toggle-button/ToggleButton';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <ToggleButton />
       <Text size="h1" component="h3">
         Home page
       </Text>
