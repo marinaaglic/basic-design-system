ebdfc6b65dbe7d76f03353163498d11680c03e7a
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-09-03 16:30:25 +0200

    styling fix for default primary

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,17 +1,22 @@
 import { IButtonProps } from './Button.types';
 import styles from './Button.module.scss';
+import clsx from 'clsx';
 
 export default function Button({
   variant,
   type,
   size,
-  icon,
   children,
-  component = 'a',
+  component = 'button',
   ...rest
 }: IButtonProps) {
   const Tag = component;
-  const buttonClass = `${styles[`${variant}-${type}`]} ${styles[size]}`;
+  const buttonClass = clsx(
+    styles.button,
+    styles[`${variant}-${type}`],
+    styles[size]
+  );
+
   return (
     <Tag className={buttonClass} {...rest}>
       {children}
--- components/button/Button.types.ts
+++ components/button/Button.types.ts
@@ -1,14 +1,12 @@
 export type ButtonColor = 'primary' | 'secondary';
 export type ButtonType = 'default' | 'transparent';
 export type ButtonSize = 'medium' | 'small';
-export type ButtonIconPosition = 'left' | 'right' | 'none';
 export type ButtonComponent = 'a' | 'button';
 
 export interface IButtonProps extends React.HTMLAttributes<HTMLElement> {
   variant: ButtonColor;
   type: ButtonType;
   size: ButtonSize;
-  icon?: ButtonIconPosition;
   component?: ButtonComponent;
   disabled?: boolean;
 }
