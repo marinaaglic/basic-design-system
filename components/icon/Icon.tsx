aca2a713b8325d7eb9d39d673a35405b5c03bfc3
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-09-08 17:26:22 +0200

    added icon utils file

--- components/icon/Icon.tsx
+++ components/icon/Icon.tsx
@@ -1,20 +1,8 @@
 import { IIconProps } from './Icon.types';
 import styles from './Icon.module.scss';
-import SearchIcon from '../../public/assets/search.svg';
-import CloseIcon from '../../public/assets/close.svg';
-import DarkIcon from '../../public/assets/dark.svg';
-import LightIcon from '../../public/assets/light.svg';
-import ArrowRightIcon from '../../public/assets/arrow-right.svg';
+import { icons } from './Icon.utils';
 
 export default function Icon({ size, type, className, ...rest }: IIconProps) {
-  const icons = {
-    search: SearchIcon,
-    close: CloseIcon,
-    dark: DarkIcon,
-    light: LightIcon,
-    arrowRight: ArrowRightIcon,
-  };
-
   const IconComponent = icons[type];
   return (
     <span className={`${styles.iconWrapper} ${className}`} {...rest}>
---
+++ components/icon/Icon.utils.ts
@@ -0,0 +1,13 @@
+import SearchIcon from '../../public/assets/search.svg';
+import CloseIcon from '../../public/assets/close.svg';
+import DarkIcon from '../../public/assets/dark.svg';
+import LightIcon from '../../public/assets/light.svg';
+import ArrowRightIcon from '../../public/assets/arrow-right.svg';
+
+export const icons = {
+  search: SearchIcon,
+  close: CloseIcon,
+  dark: DarkIcon,
+  light: LightIcon,
+  arrowRight: ArrowRightIcon,
+};
